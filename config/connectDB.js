59e7bbab8519a2d69fe06618b8b04fa6a63be69c
Abenezer <abentad4@gmail.com>
2020-12-28 22:15:58 +0300

    after making mongoose connect with a string instead of the .env

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 export default async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://Ab:19875321ab@restapi.dgg3t.mongodb.net/restApi?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
     console.log(`Connected to mongoDB database ${conn.connection.name}`);
   } catch (error) {
     //will log the error to the console and quits the server
